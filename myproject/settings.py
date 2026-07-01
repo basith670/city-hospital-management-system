7984456f5ca3dc5f7375bf616ee1fd5ca6c55592
basith670 <basithkdrmf87@gmail.com>
2026-07-01 14:08:47 +0530

    Update SECRET_KEY fallback

--- myproject/settings.py
+++ myproject/settings.py
@@ -21,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "6zpaemt)pdn!74#3pvkv%abl8ipne*zccku!^!%q9dkg=ox84a"
+)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = [
     ".onrender.com",
